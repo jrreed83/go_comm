08f68698e8e7c11a6cec65166cd3cd3c1fb9cbdf
jrreed83 <joeyrreed@gmail.com>
2016-12-07 14:52:35 -0800

    jrr

--- digitalcircuits/counter.go
+++ digitalcircuits/counter.go
@@ -1,18 +1,35 @@
-package hardware
+package digitalcircuits
 
-func counter(clk chan uint8, reset chan uint8, enable chan uint8, output chan uint8) {
-	var cnt uint = 0
+func d_flip_flop(clk chan uint8, data chan uint8, output chan uint8) {
+	var state uint8 = 0
+	var prv_clk uint8 = 0
+
 	for {
-		c := <-clk
-		e := <-enable
-		r := <-reset
-		if 1 == c {
-			if 1 == e {
+		curr_clk := <-clk
+		curr_data := <-data
+
+		if 1 == curr_clk && prv_clk == 0 {
+			output <- state
+			state = curr_data
+		}
+	}
+}
+
+func counter(clk chan uint8, enable chan uint8, reset chan uint8, output chan uint8) {
+	var cnt uint8 = 0
+	var prv_clk uint8 = 0
+	for {
+		curr_clk := <-clk
+		curr_en := <-enable
+		curr_rst := <-reset
+		if 1 == curr_clk && prv_clk == 0 {
+			if 1 == curr_en {
 				cnt = cnt + 1
-			} else if 1 == r {
+			} else if 1 == curr_rst {
 				cnt = 0
 			}
 		}
+		prv_clk = curr_clk
 		output <- cnt
 	}
 }

27fcc65b770c9c6292fa6f3fd928d080e2c0cff5
jrreed83 <joeyrreed@gmail.com>
2016-12-08 21:33:46 -0800

    jrr

--- digitalcircuits/digitalcircuits_test.go
+++ digitalcircuits/digitalcircuits_test.go
@@ -5,6 +5,47 @@ import (
 	"time"
 )
 
+func TestDFlipFlop(t *testing.T) {
+	clk := make(chan uint8)
+	data := make(chan uint8)
+	output := make(chan uint8)
+
+	t.Log("Kicking Off Clock")
+	go func() {
+		clk <- 0
+		clk <- 1
+		clk <- 0
+		clk <- 1
+		clk <- 0
+		clk <- 1
+	}()
+	t.Log("Kicking Off Data Line")
+	go func() {
+		data <- 23
+		data <- 45
+		data <- 65
+		data <- 52
+		data <- 12
+		data <- 10
+	}()
+
+	t.Log("Kicking off flip flop")
+	go d_flip_flop(clk, data, output)
+
+	go func() {
+		for {
+			select {
+			case y := <-output:
+				t.Log(y)
+			case <-time.After(1):
+				t.Log("Times Out ... Quitting")
+				return
+			}
+		}
+	}()
+
+}
+
 func TestClockEvent(t *testing.T) {
 	// Initialize Clock Channel
 	clk := make(chan uint8)
@@ -30,7 +71,6 @@ func TestClockEvent(t *testing.T) {
 	}()
 
 	go func() {
-
 		for {
 			select {
 			case <-event:

9a3efbb9a1512e52debf1399989f06de2a1599eb
jrreed83 <joeyrreed@gmail.com>
2016-12-23 21:16:32 -0800

    jrr

--- ringbuffer/ringbuffer.go
+++ ringbuffer/ringbuffer.go
@@ -3,41 +3,37 @@ package ringbuffer
 import "errors"
 
 type ringbuffer struct {
-	readPtr      uint32
-	writePtr     uint32
-	capacity     uint32
-	buffer       []byte
-	itemsToRead  uint32
-	spaceToWrite uint32
+	readPtr  uint32
+	writePtr uint32
+	capacity uint32
+	buffer   []byte
+	length   uint32
 }
 
 func NewRingBuffer(capacity uint32) *ringbuffer {
 	return &ringbuffer{readPtr: 0,
-		writePtr:     0,
-		capacity:     capacity,
-		buffer:       make([]byte, capacity),
-		itemsToRead:  0,
-		spaceToWrite: capacity}
+		writePtr: 0,
+		capacity: capacity,
+		buffer:   make([]byte, capacity),
+		length:   0}
 }
 
 func (r *ringbuffer) write(x byte) error {
-	if r.spaceToWrite == 0 {
+	if r.length == r.capacity {
 		return errors.New("No space to write")
 	}
 	r.buffer[r.writePtr] = x
 	r.writePtr = (r.writePtr + 1) % r.capacity
-	r.itemsToRead += 1
-	r.spaceToWrite -= 1
+	r.length += 1
 	return nil
 }
 
 func (r *ringbuffer) read() (byte, error) {
-	if r.itemsToRead == 0 {
+	if r.length == 0 {
 		return 0, errors.New("No items to read")
 	}
 	x := r.buffer[r.readPtr]
 	r.readPtr = (r.readPtr + 1) % r.capacity
-	r.itemsToRead -= 1
-	r.spaceToWrite += 1
+	r.length += 1
 	return x, nil
 }

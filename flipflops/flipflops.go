3be4ad31cce193334af9946af9d18a64abcf1a2a
jrreed83 <joeyrreed@gmail.com>
2017-01-21 07:57:23 -0800

    jrr

--- flipflops/flipflops.go
+++ flipflops/flipflops.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
-	//	"time"
+	"time"
 )
+
+type MsgType byte
 
 type Message struct {
 	Time uint32
 	Data byte
+	Type MsgType
 }
 
 type Signal struct {
 	Msg chan Message    // Informs reader processes what time-stamp to read
 	Tbl map[uint32]byte // Shared memory
 	Num int             // Number of readers/subscribers
+
+	CurrTime uint32
+	CurrVal  byte
+	NxtTime  uint32 // Most Recent Time
+	NxtVal   byte   // Most recent value
 }
 
 func NewSignal(n int) *Signal {
@@ -26,6 +35,12 @@ func NewSignal(n int) *Signal {
 }
 
 func (s *Signal) Put(t uint32, d byte) {
+	if t < s.NxtTime {
+		panic("Must be a later time")
+	}
+	s.NxtTime = t
+	s.NxtVal = d
+
 	s.Tbl[t] = d
 }
 
@@ -33,19 +48,30 @@ func (s *Signal) Get() byte {
 	return 0
 }
 
-func (s *Signal) SyncMsg(t uint32) {
+// Send most recent data to subscribers
+func (s *Signal) SyncMsg() {
 	var i int
 	for i = 0; i < s.Num; i++ {
-		s.Msg <- Message{Time: t}
+		s.Msg <- Message{Time: s.NxtTime, Data: s.NxtVal}
 	}
 }
 
-func (s *Signal) Wait() Message {
-	return <-s.Msg
+func (s *Signal) Wait() (Message, error) {
+	select {
+	case msg := <-s.Msg:
+		return msg, nil
+	case <-time.After(time.Second):
+		return Message{}, errors.New("No more signal")
+	}
 }
 
-func (s *Signal) Raise() {
-	s.Msg <- Message{}
+func (s *Signal) Raise() error {
+	select {
+	case s.Msg <- Message{}:
+		return nil
+	case <-time.After(time.Second):
+		return errors.New("Error")
+	}
 }
 
 type Clock struct {
@@ -57,10 +83,15 @@ type Clock struct {
 func (c *Clock) Start() {
 	go func() {
 		for {
-			c.Event.Wait()
+			_, err := c.Event.Wait()
+			if err != nil {
+				continue
+			}
 			pulse := uint8(c.Time % 2)
 			c.Driver.Put(c.Time, pulse)
-			c.Driver.SyncMsg(c.Time)
+
+			// Synchronize
+			c.Driver.SyncMsg()
 			c.Time++
 
 		}
@@ -72,22 +103,34 @@ func (c *Clock) Tick() {
 }
 
 func main() {
-	s := NewSignal(1)
+	drv := NewSignal(1)
+	evt := NewSignal(1)
 
+	clk := Clock{Time: 0, Event: evt, Driver: drv}
 	var wg sync.WaitGroup
 
 	wg.Add(2)
 
+	clk.Start()
+
 	go func() {
-		s.Put(0, 0)
-		s.Put(1, 4)
-		s.SyncMsg(1)
+		clk.Tick()
+		clk.Tick()
+		clk.Tick()
 		wg.Done()
 	}()
 
 	go func() {
-		fmt.Println(s.Wait())
-		wg.Done()
+		for {
+			msg, err := clk.Driver.Wait()
+			if err != nil {
+				fmt.Println(err)
+				wg.Done()
+				return
+			}
+			fmt.Println(msg)
+		}
+
 	}()
 
 	wg.Wait()

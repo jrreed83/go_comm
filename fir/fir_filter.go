0e4997d91e9338faacc062d1b423a9c773740b66
jrreed83 <joeyrreed@gmail.com>
2017-07-23 14:50:07 -0700

    jrr

--- fir/fir_filter.go
+++ fir/fir_filter.go
@@ -13,7 +13,7 @@ func NewFirFilter(coeffs []float64) *FirFilter {
 	return &FirFilter{
 		length: n,
 		coeffs: c,
-		buffer: make([]float64, n),
+		buffer: make([]float64, n-1),
 	}
 }
 
@@ -25,19 +25,43 @@ func dot(n int, x []float64, y []float64) float64 {
 	return result
 }
 
-func step(fir *FirFilter, samples []float64) []float64 {
+func directI(fir *FirFilter, samples []float64) []float64 {
 	length := fir.length
 	coeffs := fir.coeffs
 	output := make([]float64, len(samples))
 	buffer := fir.buffer
 	for i, x := range samples {
-		output[i] = dot(length, coeffs, buffer)
-
 		// Shift the values
+		result := x * coeffs[0]
+		for j := length - 1; j >= 2; j-- {
+			result += buffer[j-1] * coeffs[j]
+			buffer[j-1] = buffer[j-2]
+		}
+		result += buffer[0] * coeffs[1]
+		buffer[0] = x
+
+		output[i] = result
+	}
+
+	return output
+}
+
+func directII(fir *FirFilter, samples []float64) []float64 {
+	length := fir.length
+	coeffs := fir.coeffs
+	output := make([]float64, len(samples))
+	buffer := fir.buffer
+	for i, x := range samples {
+		// Shift the values
+		result := 0.0
 		for j := length - 1; j > 0; j-- {
+			result += buffer[j] * coeffs[j]
 			buffer[j] = buffer[j-1]
 		}
+		result += buffer[0] * coeffs[0]
 		buffer[0] = x
+
+		output[i] = result
 	}
 
 	return output
--- fir/fir_filter_test.go
+++ fir/fir_filter_test.go
@@ -6,8 +6,9 @@ import (
 )
 
 func TestFirFilter(t *testing.T) {
-	fir := NewFirFilter([]float64{0, 1, 0})
-	out := step(fir, []float64{0, 1, 1, 1, 2, 3, 1, 2, 3, 5, 3})
+	fir := NewFirFilter([]float64{0, 0, 1, 0, 0})
+	out := directI(fir, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
 	fmt.Println(out)
-
+	out = directI(fir, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	fmt.Println(out)
 }
